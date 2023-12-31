e5cc95d25c53cca4f3c48329846c8bf4f4290641
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-12-31 13:28:42 -0600

    added usage_cli lib

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,31 +1,6 @@
-#[macro_use]
-extern crate log;
+use std::env;
 
-use std::path::PathBuf;
-
-use miette::Result;
-
-use cli::Cli;
-
-mod cli;
-mod env;
-mod hash;
-mod shebang;
-
-fn main() -> Result<()> {
-    env_logger::init();
-
-    let args = std::env::args().collect::<Vec<String>>();
-    // if let Some("__USAGE__") = args.get(2).map(|s| s.as_str()) {
-    //     return split_script(&args[1]);
-    // } else if let Some(script) = args.get(1) {
-    if let Some(script) = args.get(1) {
-        if script.starts_with("./") || script.starts_with("/") {
-            let script: PathBuf = script.into();
-            if script.starts_with("./") && script.exists() {
-                return shebang::execute(&script, &args);
-            }
-        }
-    }
-    Cli::run(&args)
+fn main() -> miette::Result<()> {
+    let args: Vec<_> = env::args().collect();
+    usage_cli::run(&args)
 }
