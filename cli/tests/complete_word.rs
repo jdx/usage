87b15642c35d86477a0027dcd60cf81afffea11b
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-02-09 23:12:05 -0600

    wip

--- cli/tests/complete_word.rs
+++ cli/tests/complete_word.rs
@@ -1,3 +1,4 @@
+use assert_cmd::assert::Assert;
 use std::process::Command;
 
 use assert_cmd::prelude::*;
@@ -5,15 +6,25 @@ use predicates::prelude::*;
 
 #[test]
 fn complete() {
-    cmd()
-        .args(["plugins", "install", "pl"])
-        .assert()
-        .success()
-        .stdout(predicate::str::contains("plugin-2"));
+    assert_cmd(&["plugins", "install", "pl"]).stdout("plugin-1\nplugin-2\nplugin-3\n");
+}
+
+#[test]
+fn subcommands() {
+    assert_cmd(&["plugins", "install"]).stdout(predicate::str::contains("install"));
+}
+
+#[test]
+fn cword() {
+    assert_cmd(&["--cword=2", "plugins", "install"]).stdout(predicate::str::contains("plugin-2"));
 }
 
 fn cmd() -> Command {
     let mut cmd = Command::cargo_bin("usage").unwrap();
-    cmd.args(["cw", "-f", "../examples/basic.usage.kdl", "--"]);
+    cmd.args(["cw", "-f", "../examples/basic.usage.kdl"]);
     cmd
+}
+
+fn assert_cmd(args: &[&str]) -> Assert {
+    cmd().args(args).assert().success()
 }
