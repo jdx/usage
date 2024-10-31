5746a7fa97cc61a552b8df821c93e0863eb24094
jdx <216188+jdx@users.noreply.github.com>
2024-10-31 09:46:32 -0500

    fix: allow calling `usage g completion -f`
    Fixes #142

--- lib/src/complete/fish.rs
+++ lib/src/complete/fish.rs
@@ -1,8 +1,7 @@
-pub fn complete_fish(bin: &str, usage_cmd: &str) -> String {
-    // let usage = env::USAGE_BIN.display();
-    // let bin = &spec.bin;
-    // let raw = spec.to_string().replace('\'', r"\'").to_string();
-    format!(
+use crate::Spec;
+
+pub fn complete_fish(bin: &str, usage_cmd: Option<&str>, spec: Option<&Spec>) -> String {
+    let mut out = vec![format!(
         r#"
 # if "usage" is not installed show an error
 if ! command -v usage &> /dev/null
@@ -10,17 +9,35 @@ if ! command -v usage &> /dev/null
     echo "Error: usage CLI not found. This is required for completions to work in {bin}." >&2
     echo "See https://usage.jdx.dev for more information." >&2
     return 1
-end
+end"#
+    )];
 
-set _usage_spec_{bin} ({usage_cmd} | string collect)
-complete -xc {bin} -a '(usage complete-word --shell fish -s "$_usage_spec_{bin}" -- (commandline -cop) (commandline -t))'
-"#
-    )
+    if let Some(usage_cmd) = &usage_cmd {
+        out.push(format!(
+            r#"
+set _usage_spec_{bin} ({usage_cmd} | string collect)"#
+        ));
+    }
+
+    if let Some(spec) = &spec {
+        let spec_escaped = spec.to_string().replace("'", r"\'");
+        out.push(format!(
+            r#"
+set -x _usage_spec_{bin} '{spec_escaped}'"#
+        ));
+    }
+
+    out.push(format!(
+        r#"complete -xc {bin} -a '(usage complete-word --shell fish -s "$_usage_spec_{bin}" -- (commandline -cop) (commandline -t))'"#
+    ));
+
+    out.join("\n")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test::SPEC_KITCHEN_SINK;
     use insta::assert_snapshot;
 
     #[test]
@@ -28,7 +45,7 @@ mod tests {
         // let spec = r#"
         // "#;
         // let spec = Spec::parse(&Default::default(), spec).unwrap();
-        assert_snapshot!(complete_fish("mycli", "mycli complete --usage").trim(), @r###"
+        assert_snapshot!(complete_fish("mycli", Some("mycli complete --usage"), None).trim(), @r###"
         # if "usage" is not installed show an error
         if ! command -v usage &> /dev/null
             echo >&2
@@ -40,5 +57,48 @@ mod tests {
         set _usage_spec_mycli (mycli complete --usage | string collect)
         complete -xc mycli -a '(usage complete-word --shell fish -s "$_usage_spec_mycli" -- (commandline -cop) (commandline -t))'
         "###);
+
+        assert_snapshot!(complete_fish("mycli", None, Some(&SPEC_KITCHEN_SINK)).trim(), @r##"
+        # if "usage" is not installed show an error
+        if ! command -v usage &> /dev/null
+            echo >&2
+            echo "Error: usage CLI not found. This is required for completions to work in mycli." >&2
+            echo "See https://usage.jdx.dev for more information." >&2
+            return 1
+        end
+
+        set -x _usage_spec_mycli 'name "mycli"
+        bin "mycli"
+        source_code_link_template "https://github.com/jdx/mise/blob/main/src/cli/{{path}}.rs"
+        flag "--flag1" help="flag1 description"
+        flag "--flag2" help="flag2 description" {
+            long_help "flag2 long description"
+        }
+        flag "--flag3" help="flag3 description" negate="--no-flag3"
+        flag "--shell" {
+            arg "<shell>" {
+                choices "bash" "zsh" "fish"
+            }
+        }
+        arg "<arg1>" help="arg1 description"
+        arg "<arg2>" help="arg2 description" default="default value" {
+            choices "choice1" "choice2" "choice3"
+        }
+        arg "<arg3>" help="arg3 description" help_long="arg3 long description"
+        arg "<argrest>..." var=true
+        cmd "plugin" {
+            cmd "install" {
+                flag "-g --global"
+                flag "-d --dir" {
+                    arg "<dir>"
+                }
+                flag "-f --force" negate="--no-force"
+                arg "<plugin>"
+                arg "<version>"
+            }
+        }
+        '
+        complete -xc mycli -a '(usage complete-word --shell fish -s "$_usage_spec_mycli" -- (commandline -cop) (commandline -t))'
+        "##);
     }
 }
