44f5168c76492c22560ffd87d8f372dbd7f2d4b1
Risu <79110363+risu729@users.noreply.github.com>
2025-07-17 05:29:49 +1000

    Revert "Revert "fix(completions): ignore aliases and functions named usage" (…"
    This reverts commit a5ed8bb965c5fc4abcac4fbb83cb59ec4868fbda.

--- cli/assets/completions/usage.fish
+++ cli/assets/completions/usage.fish
@@ -1,5 +1,5 @@
 # if "usage" is not installed show an error
-if ! command -v usage &> /dev/null
+if ! command usage &> /dev/null
     echo >&2
     echo "Error: usage CLI not found. This is required for completions to work in usage." >&2
     echo "See https://usage.jdx.dev for more information." >&2
@@ -9,7 +9,7 @@ end
 set _usage_spec_usage (usage --usage-spec | string collect)
 set -l tokens
 if commandline -x >/dev/null 2>&1
-    complete -xc usage -a '(usage complete-word --shell fish -s "$_usage_spec_usage" -- (commandline -xpc) (commandline -t))'
+    complete -xc usage -a '(command usage complete-word --shell fish -s "$_usage_spec_usage" -- (commandline -xpc) (commandline -t))'
 else
-    complete -xc usage -a '(usage complete-word --shell fish -s "$_usage_spec_usage" -- (commandline -opc) (commandline -t))'
+    complete -xc usage -a '(command usage complete-word --shell fish -s "$_usage_spec_usage" -- (commandline -opc) (commandline -t))'
 end
--- lib/src/complete/zsh.rs
+++ lib/src/complete/zsh.rs
@@ -38,7 +38,7 @@ _{bin_snake}() {{
   typeset -A opt_args
   local curcontext="$curcontext" spec cache_policy
 
-  if ! command -v {usage_bin} &> /dev/null; then
+  if ! command {usage_bin} &> /dev/null; then
       echo >&2
       echo "Error: {usage_bin} CLI not found. This is required for completions to work in {bin}." >&2
       echo "See https://usage.jdx.dev for more information." >&2
@@ -74,7 +74,7 @@ __USAGE_EOF__"#,
 
     out.push(format!(
         r#"
-  _arguments "*: :(($({usage_bin} complete-word --shell zsh -s "$spec" -- "${{words[@]}}" )))"
+  _arguments "*: :(($(command {usage_bin} complete-word --shell zsh -s "$spec" -- "${{words[@]}}" )))"
   return 0
 }}
 
