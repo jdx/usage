d427df2963c2a9a1959bbc0cfe85df8e72290120
default <216188+jdx@users.noreply.github.com>
2026-08-17 03:41:38 +0000

    refactor(cli): use usage value hints

--- cli/src/cli/generate/fig.rs
+++ cli/src/cli/generate/fig.rs
@@ -53,7 +53,11 @@ pub struct Fig {
     file: Option<PathBuf>,
 
     /// File path where the generated Fig spec will be saved, or "-" for stdout
-    #[usage(long, value_hint = clap::ValueHint::FilePath, effect = "write")]
+    #[usage(
+        long,
+        value_hint = usage_argv::ValueHint::FilePath,
+        effect = "write"
+    )]
     out_file: Option<PathBuf>,
 
     /// Raw string spec input
